1eed7c455e20e8ae44464d78122fd4f0f7e2bce2
Anthony Freda <anthonyfreda323@gmail.com>
2018-12-20 14:04:23 -0500

    set up create user

--- controllers/user.go
+++ controllers/user.go
@@ -1,10 +1,18 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"todo/models"
+	"todo/utils"
+)
 
 // SignUp - POST /api/v1/users/signup
 func SignUp(w http.ResponseWriter, r *http.Request) {
-
+	user := &models.User{}
+	json.NewDecoder(r.Body).Decode(user) // pipe body into user model
+	resp := user.CreateUser()            // attempt to create user
+	utils.Respond(w, resp)               // send res
 }
 
 // LogIn - POST /api/v1/users/login
--- models/user.go
+++ models/user.go
@@ -1,8 +1,11 @@
 package models
 
 import (
+	"os"
 	"strings"
 	"todo/utils"
+
+	"golang.org/x/crypto/bcrypt"
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/jinzhu/gorm"
@@ -19,7 +22,7 @@ type User struct {
 	gorm.Model
 	Email    string `json:"email"`
 	Password string `json:"password"`
-	Token    string `json:"token";gorm:"-"`
+	Token    string `json:"token";sql:"-"`
 }
 
 // Validate if all values are present
@@ -50,6 +53,27 @@ func (u *User) CreateUser() map[string]interface{} {
 	if message, ok := u.Validate(); !ok {
 		return message
 	}
+
+	hashed, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return utils.Message(false, "Something went wrong")
+	}
+
+	u.Password = string(hashed) // replace plain pw with hashed one
+	GetDB().Create(u)           // save to db
+	u.Password = ""             // dont send password back
+
+	tk := &Token{UserID: u.ID}
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+	signed, _ := token.SignedString([]byte(os.Getenv("token_password")))
+
+	u.Token = signed
+
+	resp := utils.Message(true, "User created")
+	resp["user"] = u
+
+	return resp
 }
 
 // Login user, get jwt
